0cec018ca74c58e5ee86e1f64276476122ff621d
G0oCe <alim.sadigov20@gmail.com>
2025-01-22 12:39:12 +0400

    implement basic server logic

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+TcpListener server = new TcpListener(IPAddress.Any, 5000);
+server.Start();
+Console.WriteLine("Server started...");
+
+while (true)
+{
+	TcpClient client = server.AcceptTcpClient();
+	Console.WriteLine("Client connected!");
+
+	NetworkStream stream = client.GetStream();
+	byte[] buffer = new byte[1024];
+	int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+	string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+	Console.WriteLine($"Received: {message}");
+
+	byte[] response = Encoding.UTF8.GetBytes("Message received");
+	stream.Write(response, 0, response.Length);
+
+	client.Close();
+}
