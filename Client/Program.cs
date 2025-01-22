e861384dfc2e624d499668a46b3317e20cea3b79
G0oCe <alim.sadigov20@gmail.com>
2025-01-22 12:43:46 +0400

    implement basic clinet logic

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Net.Sockets;
+using System.Text;
 
-app.MapGet("/", () => "Hello World!");
+TcpClient client = new TcpClient("127.0.0.1", 5000);
+NetworkStream stream = client.GetStream();
 
-app.Run();
+string message = "Hello, Server!";
+byte[] data = Encoding.UTF8.GetBytes(message);
+stream.Write(data, 0, data.Length);
+
+byte[] buffer = new byte[1024];
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine($"Server response: {response}");
+
+client.Close();
